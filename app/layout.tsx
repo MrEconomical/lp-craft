6ea3a1dfe191db5ce2b72334968df9b5a3a8b8a5
MrEconomical <47700125+MrEconomical@users.noreply.github.com>
2024-07-28 10:40:45 -0700

    add home page

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+    title: "LP Craft",
+    description: "A linear program crafting XP optimizer",
+}
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    )
+}
