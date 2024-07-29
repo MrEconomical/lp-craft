66a68e4a7f95a76d7eae5fed861df9c23abb99cb
MrEconomical <47700125+MrEconomical@users.noreply.github.com>
2024-07-28 17:08:36 -0700

    add types

--- app/api/recipes/route.ts
+++ app/api/recipes/route.ts
@@ -1,13 +1,41 @@
-import { NextRequest } from "next/server"
+interface BaseArtifact {
+    afx_id: number,
+    afx_level: number,
+}
+interface Ingredient extends BaseArtifact {
+    count: number,
+}
+interface Artifact extends BaseArtifact {
+    id: string,
+    recipe: {
+        ingredients: Array<Ingredient>,
+    },
+}
 
-export type Recipes = Record<string, {
-    ingredients: Record<string, number>,
-    xp: number,
-}>
+export type Recipes = {
+    [key: number]: {
+        ingredients: Record<number, number>,
+        xp: number,
+    } | null,
+}
+export type NameTable = Record<number, string>
+
+export const revalidate = 3600 // 1 hour
 
 /**
- * Fetches crafting recipes from the wasmegg GitHub and constructs a table.
+ * Fetches crafting recipes from the wasmegg GitHub and constructs a data table.
  */
-export async function GET(request: NextRequest) {
-    return new Response("{}", { status: 200 })
+export async function GET() {
+
+    return new Response(JSON.stringify({
+        recipes: {},
+        names: {},
+    }), { status: 200 })
+}
+
+/**
+ * Computes a unique numerical hash for an artifact.
+ */
+function hashArtifact(artifact: BaseArtifact) {
+    return artifact.afx_id * 4 + artifact.afx_level
 }
