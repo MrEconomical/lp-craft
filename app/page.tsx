9cb92ef099113643f2d3d4fdce5bf2e5038caf68
MrEconomical <47700125+MrEconomical@users.noreply.github.com>
2024-07-28 19:53:57 -0700

    load highs on the client side

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,28 @@
 import Script from "next/script"
 import { JSX, useState, useEffect } from "react"
 
+async function optimizeCrafts(highs: any) {
+    console.log("optimizing")
+    console.log(highs)
+
+    const PROBLEM = `Maximize
+    obj:
+        x1 + 2 x2 + 4 x3 + x4
+    Subject To
+    c1: - x1 + x2 + x3 + 10 x4 <= 20
+    c2: x1 - 4 x2 + x3 <= 30
+    c3: x2 - 0.5 x4 = 0
+    Bounds
+    0 <= x1 <= 40
+    2 <= x4 <= 3
+    End`
+    console.log(highs.solve(PROBLEM))
+}
+
 export default function Home(): JSX.Element {
     const [ highs, setHighs ] = useState(null)
 
+    // Load the highs solver on the client side
     useEffect(() => {
         async function loadHighs() {
             const highs = await (window as any).Module()
@@ -22,6 +41,7 @@ export default function Home(): JSX.Element {
                 strategy="beforeInteractive"
             ></Script>
             {!highs ? "no highs" : "highs"}
+            <button onClick={() => optimizeCrafts(highs)}>Calculate</button>
         </>
     )
 }
