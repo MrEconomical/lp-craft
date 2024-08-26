a324b3ba94044d8525be4615cb37cd1cf01a4919
MrEconomical <47700125+MrEconomical@users.noreply.github.com>
2024-08-25 17:35:34 -0700

    add data exports

--- data/names.ts
+++ data/names.ts
@@ -1,3 +1,11 @@
+/**
+ * Exports a mapping from artifact IDs to names.
+ */
+
+import nameTable from "./names.json"
+
 export interface ArtifactNames {
     [id: number]: string,
 }
+
+export const names = nameTable as ArtifactNames
--- data/recipes.ts
+++ data/recipes.ts
@@ -1,6 +1,14 @@
+/**
+ * Exports a mapping from artifact names to crafting info.
+ */
+
+import recipeData from "./recipes.json"
+
 export interface Recipes {
     [artifact: string]: {
         ingredients: Record<string, number>,
         xp: number,
     } | null,
 }
+
+export const recipes = recipeData as Recipes
