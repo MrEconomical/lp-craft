e6e0c2f1b4d9d39feb3f59e7d687a2abee5b5292
MrEconomical <47700125+MrEconomical@users.noreply.github.com>
2024-08-25 20:25:32 -0700

    add problem

--- app/optimize.ts
+++ app/optimize.ts
@@ -27,12 +27,30 @@ export function optimizeCrafts(highs: Highs, inventory: Inventory): Solution {
  * Generates a linear program problem in CPLEX format.
  */
 function getProblem(inventory: Inventory): string {
-    const lines = []
-    const artifacts = Object.keys(recipes)
-
     // Generate the maximum XP objective
+    const lines = []
     lines.push("Maximize")
     lines.push(`  obj: ${getObjective(recipes)}`)
+
+    // Add a resource constraint for each artifact
+    lines.push("Subject To")
+    for (const artifact in recipes) {
+        const constraint = getConstraint(recipes, inventory, artifact)
+        if (constraint) {
+            lines.push(`  c_${artifact}: ${constraint}`)
+        }
+    }
+
+    // Restrict craft counts to positive numbers
+    lines.push("Bounds")
+    for (const artifact in recipes) {
+        lines.push(`  ${artifact} >= 0`)
+    }
+
+    // Specify all variables as integers
+    lines.push("General")
+    lines.push(`  ${Object.keys(recipes).join(" ")}`)
+    lines.push("End")
 
     return lines.join("\n")
 }
@@ -43,10 +61,32 @@ function getProblem(inventory: Inventory): string {
 function getObjective(recipes: Recipes): string {
     const crafts = []
     for (const artifact in recipes) {
-        if (!recipes[artifact]) {
-            continue
+        if (recipes[artifact]) {
+            crafts.push(`${recipes[artifact].xp} ${artifact}`)
         }
-        crafts.push(`${recipes[artifact].xp} ${artifact}`)
     }
     return crafts.join(" + ")
+}
+
+/**
+ * Generates a resource constraint inequality for an artifact. The total quantity
+ * used in each craft that uses the artifact must be bounded by the inventory count
+ * plus the number crafted.
+ */
+function getConstraint(recipes: Recipes, inventory: Inventory, artifact: string): string | null {
+    const used = []
+    for (const parent in recipes) {
+        if (recipes[parent] && artifact in recipes[parent].ingredients) {
+            used.push(`${recipes[parent].ingredients[artifact]} ${parent}`)
+        }
+    }
+    if (used.length == 0) {
+        return null
+    }
+
+    const available = inventory[artifact] || 0
+    if (recipes[artifact]) {
+        return `${used.join(" + ")} - ${artifact} <= ${available}`
+    }
+    return `${used.join(" + ")} <= ${available}`
 }
