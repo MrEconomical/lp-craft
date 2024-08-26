0c2c5621e8ae9efe5b7ab0f6c02212c7f0958207
MrEconomical <47700125+MrEconomical@users.noreply.github.com>
2024-08-26 11:02:23 -0700

    add mission export

--- data/missions.ts
+++ data/missions.ts
@@ -2,8 +2,12 @@
  * Exports an array of ship target drop statistics.
  */
 
+import missionData from "./missions.json"
+
 export interface Mission {
     ship: string,
     target: string | null,
     rates: Record<string, number>,
 }
+
+export const missions = missionData as Mission[]
