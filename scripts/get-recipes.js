a22a8c3461e1580b829b9d9b2549391760e8937d
MrEconomical <47700125+MrEconomical@users.noreply.github.com>
2024-08-25 17:24:36 -0700

    add recipes file

--- scripts/get-recipes.js
+++ scripts/get-recipes.js
@@ -12,20 +12,34 @@ const RECIPES_URL = "https://raw.githubusercontent.com/carpetsage/egg/main/wasme
 const recipeData = await fetch(RECIPES_URL).then(response => response.json())
 const artifacts = recipeData.artifact_families.flatMap(family => family.tiers)
 
-// Compute a mapping from artifact IDs to names
+// Compute artifact names and crafting info
 const names = {}
-const keys = []
+const recipes = {}
+
 for (const artifact of artifacts) {
     const key = hashArtifact(artifact)
-    names[key] = formatName(artifact.id)
-    keys.push(key)
+    const name = formatName(artifact.id)
+    names[key] = name
+
+    if (!artifact.recipe) {
+        recipes[name] = null
+        continue
+    }
+    recipes[name] = {
+        ingredients: {},
+        xp: artifact.crafting_xp,
+    }
+    for (const ingredient of artifact.recipe.ingredients) {
+        const ingredientName = formatName(ingredient.id)
+        recipes[name].ingredients[ingredientName] = ingredient.count
+    }
 }
-keys.sort((a, b) => names[a] < names[b] ? -1 : 1)
 
 // Save artifact data to JSON files
 const NAMES_FILE = "data/names.json"
 const RECIPES_FILE = "data/recipes.json"
-fs.writeFileSync(NAMES_FILE, JSON.stringify(names, keys, 4))
+fs.writeFileSync(NAMES_FILE, JSON.stringify(names, null, 4))
+fs.writeFileSync(RECIPES_FILE, JSON.stringify(recipes, null, 4))
 
 /**
  * Computes a unique numerical hash for an artifact.
